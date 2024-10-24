e7fef4764d2e1a79ee07b14709a59f050eeb83df
Garima Uikey <150472521+GarimaUikey@users.noreply.github.com>
2024-10-24 22:34:11 +0530

    Add files via upload

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import pic from "../assets/pic.jpg";
+import p2 from "../assets/p2.jpg";
+import p3 from "../assets/p3.jpg";
+import p4 from "../assets/p4.jpg";
+import p5 from "../helpers/p5.jpg";
+import p6 from "../helpers/p6.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: pic,
+    price: 15.99,
+  },
+  {
+    name: "Margherita Pizza",
+    image: p2,
+    price: 11.99,
+  },
+  {
+    name: "PedroTech Special Pizza",
+    image: p3,
+    price: 256.53,
+  },
+  {
+    name: "Vegan Pizza",
+    image: p4,
+    price: 17.99,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: p5,
+    price: 4.99,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: p6,
+    price: 1997.99,
+  },
+];
